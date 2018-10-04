355fcb4bf662e765986af2a92e619b6905a4208c
Aleksey Pirogov <astynax@users.noreply.github.com>
2018-10-04 12:17:22 +0300

    Rework example TOML in tests using EDSL, export defaultOptions

--- src/Toml/Printer.hs
+++ src/Toml/Printer.hs
@@ -4,6 +4,7 @@
 
 module Toml.Printer
        ( PrintOptions(..)
+       , defaultOptions
        , pretty
        , prettyOptions
        , prettyTomlInd
--- test/Test/Toml/Printer/Golden.hs
+++ test/Test/Toml/Printer/Golden.hs
@@ -6,44 +6,39 @@ module Test.Toml.Printer.Golden
 
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.Silver (goldenVsAction)
-import Toml (AnyValue (..), TOML (..), Value (..))
+import Toml (TOML)
+import Toml.Edsl ((=:), mkToml, table)
 import Toml.PrefixTree ((<|))
-import Toml.Printer (PrintOptions (..), pretty, prettyOptions)
-
-import qualified Data.HashMap.Strict as HashMap
-import qualified Toml.PrefixTree as PrefixTree
+import Toml.Printer (PrintOptions (..), defaultOptions, prettyOptions)
 
 example :: TOML
-example = mempty
-    { tomlPairs  = HashMap.fromList
-          [ ("b", AnyValue $ Text "bb")
-          , ("a", AnyValue $ Text "a")
-          , ("d", AnyValue $ Text "ddd")
-          , ("c", AnyValue $ Text "cccc") ]
-    , tomlTables = PrefixTree.fromList
-          [ ( "qux" <| "doo"
-            , mempty
-                  { tomlPairs = HashMap.fromList
-                        [ ("spam", AnyValue $ Text "!")
-                        , ("egg", AnyValue $ Text "?") ]
-                  }
-            )
-          , ("foo", mempty)
-          , ("doo", mempty)
-          , ("baz", mempty)
-          ]
+example = mkToml $ do
+    "b" =: "bb"
+    "a" =: "a"
+    "d" =: "ddd"
+    "c" =: "cccc"
+    table ("qux" <| "doo") $ do
+      "spam" =: "!"
+      "egg" =: "?"
+    table "foo" $ pure ()
+    table "doo" $ pure ()
+    table "baz" $ pure ()
+
+noFormatting :: PrintOptions
+noFormatting = PrintOptions
+    { shouldSort = False
+    , indent     = 0
     }
 
 test_prettyGolden :: TestTree
 test_prettyGolden =
     testGroup "Toml.Printer"
-        [ test pretty "test/golden/pretty_default.golden"
-        , test (prettyOptions $ PrintOptions True 0)
-              "test/golden/pretty_sorted_only.golden"
-        , test (prettyOptions $ PrintOptions False 4)
-              "test/golden/pretty_indented_only.golden"
-        , test (prettyOptions $ PrintOptions False 0)
-              "test/golden/pretty_unformatted.golden"
+        [ test "pretty_default" defaultOptions
+        , test "pretty_sorted_only" noFormatting{ shouldSort = True} 
+        , test "pretty_indented_only" noFormatting{ indent = 4 }
+        , test "pretty_unformatted" noFormatting
         ]
   where
-    test prn g = goldenVsAction "example" g (pure $ prn example) id
+    test name options =
+        goldenVsAction name ("test/golden/" ++ name ++ ".golden")
+            (pure $ prettyOptions options example) id
