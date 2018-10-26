713f23258ca2f1d1a31bc6d360269aad06ce0608
Jie <jie.gillet@gmail.com>
2018-10-26 01:06:47 -0700

    [#118] more container bimaps (#120)
    * Added containers dependency
    
    * Added _Int and 4 list-type bimaps
    
    * used nonEmpty and cosmetic changes
    
    * Added ScopedTypeVariables to default extensions and removed from Combinators.hs
    
    * Made _BoundedInteger polymorphic

--- src/Toml/BiMap.hs
+++ src/Toml/BiMap.hs
@@ -1,6 +1,6 @@
-{-# LANGUAGE DataKinds      #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE Rank2Types     #-}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE KindSignatures      #-}
+{-# LANGUAGE Rank2Types          #-}
 
 {- | Implementation of partial bidirectional mapping as a data type.
 -}
@@ -33,13 +33,18 @@ module Toml.BiMap
        , _Show
        , _NaturalInteger
        , _Natural
-       , _WordInteger
+       , _BoundedInteger
        , _Word
+       , _Int
        , _Float
        , _ByteStringText
        , _ByteString
        , _LByteStringText
        , _LByteString
+       , _Set
+       , _IntSet
+       , _HashSet
+       , _NonEmpty
 
        , _Left
        , _Right
@@ -56,6 +61,7 @@ import Data.ByteString (ByteString)
 import Data.Text (Text)
 import Data.Word (Word)
 import Numeric.Natural (Natural)
+import Data.Hashable (Hashable)
 import Text.Read (readMaybe)
 import Data.Time (Day, LocalTime, TimeOfDay, ZonedTime)
 
@@ -69,6 +75,10 @@ import qualified Data.Text.Encoding as T
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.Encoding as TL
+import qualified Data.Set as S
+import qualified Data.HashSet as HS
+import qualified Data.IntSet as IS
+import qualified Data.List.NonEmpty as NE
 
 ----------------------------------------------------------------------------
 -- BiMap concepts and ideas
@@ -157,23 +167,27 @@ _Text = mkAnyValueBiMap matchText Text
 
 _ZonedTime :: BiMap ZonedTime AnyValue
 _ZonedTime = mkAnyValueBiMap (matchDate >=> getTime) (Date . Zoned)
-  where getTime (Zoned z) = Just z
-        getTime _         = Nothing
+  where
+    getTime (Zoned z) = Just z
+    getTime _         = Nothing
 
 _LocalTime :: BiMap LocalTime AnyValue
 _LocalTime = mkAnyValueBiMap (matchDate >=> getTime) (Date . Local)
-  where getTime (Local l) = Just l
-        getTime _         = Nothing
+  where
+    getTime (Local l) = Just l
+    getTime _         = Nothing
 
 _Day :: BiMap Day AnyValue
 _Day = mkAnyValueBiMap (matchDate >=> getTime) (Date . Day)
-  where getTime (Day d) = Just d
-        getTime _       = Nothing
+  where
+    getTime (Day d) = Just d
+    getTime _       = Nothing
 
 _TimeOfDay :: BiMap TimeOfDay AnyValue
 _TimeOfDay = mkAnyValueBiMap (matchDate >=> getTime) (Date . Hours)
-  where getTime (Hours h) = Just h
-        getTime _         = Nothing
+  where
+    getTime (Hours h) = Just h
+    getTime _         = Nothing
 
 _StringText :: BiMap String Text
 _StringText = iso T.pack T.unpack
@@ -189,38 +203,47 @@ _Show = _ShowString >>> _String
 
 _NaturalInteger :: BiMap Natural Integer
 _NaturalInteger = BiMap (Just . toInteger) maybeInteger
-  where maybeInteger :: Integer -> Maybe Natural
-        maybeInteger n
-          | n < 0     = Nothing
-          | otherwise = Just (fromIntegral n)
+  where
+    maybeInteger :: Integer -> Maybe Natural
+    maybeInteger n
+      | n < 0     = Nothing
+      | otherwise = Just (fromIntegral n)
 
 _Natural :: BiMap Natural AnyValue
 _Natural = _NaturalInteger >>> _Integer
 
-_WordInteger :: BiMap Word Integer
-_WordInteger = BiMap (Just . toInteger) maybeInteger
-  where maybeInteger :: Integer -> Maybe Word
-        maybeInteger n
-          | n < toInteger (minBound :: Word) = Nothing
-          | n > toInteger (maxBound :: Word) = Nothing
-          | otherwise                        = Just (fromIntegral n)
+_BoundedInteger :: (Integral a, Bounded a) => BiMap a Integer
+_BoundedInteger = BiMap (Just . toInteger) maybeBounded
+  where
+    maybeBounded :: forall a. (Integral a, Bounded a) => Integer -> Maybe a
+    maybeBounded n
+      | n < toInteger (minBound :: a) = Nothing
+      | n > toInteger (maxBound :: a) = Nothing
+      | otherwise                     = Just (fromIntegral n)
 
 _Word :: BiMap Word AnyValue
-_Word = _WordInteger >>> _Integer
+_Word = _BoundedInteger >>> _Integer
+
+_Int :: BiMap Int AnyValue
+_Int = _BoundedInteger >>> _Integer
 
 _Float :: BiMap Float AnyValue
 _Float = iso realToFrac realToFrac >>> _Double
 
 _ByteStringText :: BiMap ByteString Text
 _ByteStringText = prism T.encodeUtf8 maybeText
-  where maybeText = either (const Nothing) Just . T.decodeUtf8'
+  where
+    maybeText :: ByteString -> Maybe Text
+    maybeText = either (const Nothing) Just . T.decodeUtf8'
 
 _ByteString:: BiMap ByteString AnyValue
 _ByteString = _ByteStringText >>> _Text
 
 _LByteStringText :: BiMap BL.ByteString Text
 _LByteStringText = prism (TL.encodeUtf8 . TL.fromStrict) maybeText
-  where maybeText = either (const Nothing) (Just . TL.toStrict) . TL.decodeUtf8'
+  where
+    maybeText :: BL.ByteString -> Maybe Text
+    maybeText = either (const Nothing) (Just . TL.toStrict) . TL.decodeUtf8'
 
 _LByteString:: BiMap BL.ByteString AnyValue
 _LByteString = _LByteStringText >>> _Text
@@ -231,6 +254,18 @@ _Array elementBimap = BiMap
     { forward = mapM (forward elementBimap) >=> fmap AnyValue . toMArray
     , backward = \(AnyValue val) -> matchArray (backward elementBimap) val
     }
+
+_NonEmpty :: BiMap a AnyValue -> BiMap (NE.NonEmpty a) AnyValue
+_NonEmpty bimap = BiMap (Just . NE.toList) NE.nonEmpty >>> _Array bimap
+
+_Set :: (Ord a) => BiMap a AnyValue -> BiMap (S.Set a) AnyValue
+_Set bimap = iso S.toList S.fromList >>> _Array bimap
+
+_HashSet :: (Eq a, Hashable a) => BiMap a AnyValue -> BiMap (HS.HashSet a) AnyValue
+_HashSet bimap = iso HS.toList HS.fromList >>> _Array bimap
+
+_IntSet :: BiMap IS.IntSet AnyValue
+_IntSet = iso IS.toList IS.fromList >>> _Array _Int
 
 -- TODO: move somewhere else?
 {- | Function for creating 'Array' from list of 'AnyValue'.
