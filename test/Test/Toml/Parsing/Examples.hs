72b08ea6db099f2e1616b7b40b75f5a0dd6b12ec
Jérémie Gillet <jie.gillet@gmail.com>
2019-07-10 21:02:30 +0900

    listDirectory instead of hardcoded paths

--- test/Test/Toml/Parsing/Examples.hs
+++ test/Test/Toml/Parsing/Examples.hs
@@ -1,26 +1,21 @@
 module Test.Toml.Parsing.Examples where
 
 import Data.Either (isRight)
-import Test.Tasty.Hspec (Spec, describe, it, shouldReturn)
+import System.Directory (listDirectory)
+import Test.Tasty.Hspec (Spec, describe, it, runIO, shouldBe)
 import Toml.Parser (parse)
 
 import qualified Data.Text.IO as TIO
 
 spec_Examples :: Spec
-spec_Examples = describe "Can parse official TOML examples" $
+spec_Examples = describe "Can parse official TOML examples" $ do
+    files <- runIO $ listDirectory exampleDir
     mapM_ example files
 
 example :: FilePath -> Spec
-example file = it ("can parse file " ++ file) $
-      (isRight . parse <$> TIO.readFile ("test/examples/" ++ file))
-      `shouldReturn` True
+example file = it ("can parse file " ++ file) $ do
+    toml <- TIO.readFile (exampleDir ++ file)
+    isRight (parse toml) `shouldBe` True
 
-files :: [FilePath]
-files =
-    [ "fruit.toml"
-    , "example-v0.3.0.toml"
-    , "example-v0.4.0.toml"
-    , "example.toml"
-    , "hard_example.toml"
-    , "hard_example_unicode.toml"
-    ]
+exampleDir :: FilePath
+exampleDir = "test/examples/"
