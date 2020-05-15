5527b9ce012b0c08114d23b6fa970acbedd1fcc9
Dmitrii Kovanikov <kovanikov@gmail.com>
2020-05-15 17:42:59 +0100

    Simplify testing structure

--- test/Test/Toml/Type/PrefixTree.hs
+++ test/Test/Toml/Type/PrefixTree.hs
@@ -28,30 +28,29 @@ prefixTreeUnitSpec = describe "Unit tests for basic cases" $ do
     let c  = "c" :|| []
     let ab = "a" :|| ["b"]
 
-    describe "Insert and lookup unit tests" $ do
-        it "Lookup on empty map returns Nothing" $
-            Prefix.lookup @Bool a mempty `shouldBe` Nothing
-        it "Lookup in single map returns this element" $ do
-            let t = Prefix.single a True
-            Prefix.lookup a t `shouldBe` Just True
-            Prefix.lookup b t `shouldBe` Nothing
-        it "Lookup after insert returns this element" $ do
-            let t = Prefix.insert a True mempty
-            Prefix.lookup a t `shouldBe` Just True
-            Prefix.lookup b t `shouldBe` Nothing
-        it "Lookup after multiple non-overlapping inserts" $ do
-            let t = Prefix.insert a True $ Prefix.insert b False mempty
-            Prefix.lookup a t `shouldBe` Just True
-            Prefix.lookup b t `shouldBe` Just False
-            Prefix.lookup c t `shouldBe` Nothing
-        it "Prefix lookup" $ do
-            let t = Prefix.insert ab True mempty
-            Prefix.lookup a  t `shouldBe` Nothing
-            Prefix.lookup ab t `shouldBe` Just True
-        it "Composite key lookup" $ do
-            let t = Prefix.insert a True $ Prefix.insert ab False mempty
-            Prefix.lookup a  t `shouldBe` Just True
-            Prefix.lookup ab t `shouldBe` Just False
+    it "Lookup on empty map returns Nothing" $
+        Prefix.lookup @Bool a mempty `shouldBe` Nothing
+    it "Lookup in single map returns this element" $ do
+        let t = Prefix.single a True
+        Prefix.lookup a t `shouldBe` Just True
+        Prefix.lookup b t `shouldBe` Nothing
+    it "Lookup after insert returns this element" $ do
+        let t = Prefix.insert a True mempty
+        Prefix.lookup a t `shouldBe` Just True
+        Prefix.lookup b t `shouldBe` Nothing
+    it "Lookup after multiple non-overlapping inserts" $ do
+        let t = Prefix.insert a True $ Prefix.insert b False mempty
+        Prefix.lookup a t `shouldBe` Just True
+        Prefix.lookup b t `shouldBe` Just False
+        Prefix.lookup c t `shouldBe` Nothing
+    it "Prefix lookup" $ do
+        let t = Prefix.insert ab True mempty
+        Prefix.lookup a  t `shouldBe` Nothing
+        Prefix.lookup ab t `shouldBe` Just True
+    it "Composite key lookup" $ do
+        let t = Prefix.insert a True $ Prefix.insert ab False mempty
+        Prefix.lookup a  t `shouldBe` Just True
+        Prefix.lookup ab t `shouldBe` Just False
 
 prefixTreePropertySpec :: Spec
 prefixTreePropertySpec = describe "Property tests on laws and expected behaviour" $ do
