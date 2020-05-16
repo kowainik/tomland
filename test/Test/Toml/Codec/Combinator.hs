89261d3d321272d38d4a238817768eb7c0be143f
Dmitrii Kovanikov <kovanikov@gmail.com>
2020-05-16 22:08:46 +0100

    [#280] Tests for Combinator.{List,Map,Monoid} (#293)

--- test/Test/Toml/Codec/Combinator.hs
+++ test/Test/Toml/Codec/Combinator.hs
@@ -7,6 +7,9 @@ import Test.Hspec (Arg, Expectation, Spec, SpecWith, describe, it)
 import Test.Hspec.Hedgehog (hedgehog)
 
 -- import Test.Toml.BigType (bigTypeCodec, genBigType)
+import Test.Toml.Codec.Combinator.List (listSpec)
+import Test.Toml.Codec.Combinator.Map (mapSpec)
+import Test.Toml.Codec.Combinator.Monoid (monoidSpec)
 import Test.Toml.Codec.Combinator.Primitive (primitiveSpec)
 import Test.Toml.Codec.Combinator.Set (setSpec)
 import Test.Toml.Codec.Combinator.Tuple (tupleSpec)
@@ -15,6 +18,9 @@ import Toml.Codec.Code (decode, encode)
 
 combinatorSpec :: Spec
 combinatorSpec = describe "Combinator spec" $ do
+    listSpec
+    mapSpec
+    monoidSpec
     primitiveSpec
     setSpec
     tupleSpec
---
+++ test/Test/Toml/Codec/Combinator/List.hs
@@ -0,0 +1,28 @@
+module Test.Toml.Codec.Combinator.List
+    ( listSpec
+    ) where
+
+import Test.Hspec (Spec, describe)
+
+import Test.Toml.Codec.Combinator.Common (codecRoundtrip)
+
+import qualified Test.Toml.Gen as Gen
+import qualified Toml.Codec.BiMap.Conversion as Toml
+import qualified Toml.Codec.Combinator.List as Toml
+import qualified Toml.Codec.Combinator.Primitive as Toml
+
+
+listSpec :: Spec
+listSpec = describe "Combinator.List: Roundtrip tests" $ do
+    codecRoundtrip "[Int] (Array)       "
+        (Toml.arrayOf Toml._Int)
+        (Gen.genList Gen.genInt)
+    codecRoundtrip "NonEmpty Int (Array)"
+        (Toml.arrayNonEmptyOf Toml._Int)
+        (Gen.genNonEmpty Gen.genInt)
+    codecRoundtrip "[Int] (Table)       "
+        (Toml.list $ Toml.int "a")
+        (Gen.genList Gen.genInt)
+    codecRoundtrip "NonEmpty Int (Table)"
+        (Toml.nonEmpty $ Toml.int "a")
+        (Gen.genNonEmpty Gen.genInt)
