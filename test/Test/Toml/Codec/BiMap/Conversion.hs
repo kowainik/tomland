c81faf00053b8120e919c8d855ade27c4472750f
Dmitrii Kovanikov <kovanikov@gmail.com>
2020-05-16 17:26:46 +0100

    [#277] Fill missing parts in tests (#285)
    * [#277] Fill missing parts in tests
    
    Resolves #277
    
    * Apply suggestions from code review
    
    Co-authored-by: Veronika Romashkina <vrom911@gmail.com>
    
    * Add a couple more tests for KeysDiff
    
    Co-authored-by: Veronika Romashkina <vrom911@gmail.com>

--- test/Test/Toml/Codec/BiMap/Conversion.hs
+++ test/Test/Toml/Codec/BiMap/Conversion.hs
@@ -19,31 +19,45 @@ import qualified Toml.Codec.BiMap.Conversion as B
 
 conversionSpec :: Spec
 conversionSpec = describe "BiMap Rountrip Property tests" $ do
-    it "Bool" (testBiMap B._Bool G.genBool)
-    it "Integer" (testBiMap B._Integer G.genInteger)
-    it "Natural" (testBiMap B._Natural G.genNatural)
-    it "Int" (testBiMap B._Int G.genInt)
-    it "Word" (testBiMap B._Word G.genWord)
-    it "Word8" (testBiMap B._Word8 G.genWord8)
-    it "Double" testDouble
-    it "Float" (testBiMap B._Float G.genFloat)
-    it "Text" (testBiMap B._Text G.genText)
-    it "LazyText" (testBiMap B._LText G.genLText)
-    it "String" (testBiMap B._String G.genString)
-    it "Read (Integer)" (testBiMap B._Read G.genInteger)
-    it "ByteString" (testBiMap B._ByteString G.genByteString)
-    it "Lazy ByteString" (testBiMap B._LByteString G.genLByteString)
-    it "ByteStringArray" (testBiMap B._ByteStringArray G.genByteString)
-    it "Lazy ByteStringArray" (testBiMap B._LByteStringArray G.genLByteString)
-    it "ZonedTime" (testBiMap B._ZonedTime G.genZoned)
-    it "LocalTime" (testBiMap B._LocalTime G.genLocal)
-    it "TimeOfDay" (testBiMap B._TimeOfDay G.genHours)
-    it "Day" (testBiMap B._Day G.genDay)
-    it "IntSet" (testBiMap B._IntSet G.genIntSet)
-    it "Array (Day)" (testBiMap (B._Array B._Day) (G.genList G.genDay))
-    it "Set (Day)" (testBiMap (B._Set B._Day) (Gen.set G.range100 G.genDay))
-    it "NonEmpty (Day)" (testBiMap (B._NonEmpty B._Day) (G.genNonEmpty G.genDay))
-    it "HashSet (Integer)" (testBiMap (B._HashSet B._Integer) (G.genHashSet G.genInteger))
+    describe "Primitive" $ do
+        it "Bool"            $ testBiMap B._Bool G.genBool
+        it "Int"             $ testBiMap B._Int G.genInt
+        it "Word"            $ testBiMap B._Word G.genWord
+        it "Word8"           $ testBiMap B._Word8 G.genWord8
+        it "Integer"         $ testBiMap B._Integer G.genInteger
+        it "Natural"         $ testBiMap B._Natural G.genNatural
+        it "Double"          testDouble
+        it "Float"           $ testBiMap B._Float G.genFloat
+        it "Text"            $ testBiMap B._Text G.genText
+        it "LazyText"        $ testBiMap B._LText G.genLText
+        it "ByteString"      $ testBiMap B._ByteString G.genByteString
+        it "Lazy ByteString" $ testBiMap B._LByteString G.genLByteString
+        it "String"          $ testBiMap B._String G.genString
+
+    describe "Time" $ do
+        it "ZonedTime" $ testBiMap B._ZonedTime G.genZoned
+        it "LocalTime" $ testBiMap B._LocalTime G.genLocal
+        it "Day"       $ testBiMap B._Day G.genDay
+        it "TimeOfDay" $ testBiMap B._TimeOfDay G.genHours
+
+    describe "Arrays" $ do
+        it "Array (Int)"      $ testBiMap (B._Array B._Int) (G.genList G.genInt)
+        it "Array (Day)"      $ testBiMap (B._Array B._Day) (G.genList G.genDay)
+        it "NonEmpty (Int)"   $ testBiMap (B._NonEmpty B._Int) (G.genNonEmpty G.genInt)
+        it "Set (Int)"        $ testBiMap (B._Set B._Int) (G.genSet G.genInt)
+        it "HashSet (Int)"    $ testBiMap (B._HashSet B._Int) (G.genHashSet G.genInt)
+        it "IntSet"           $ testBiMap B._IntSet G.genIntSet
+        it "ByteStringArray"  $ testBiMap B._ByteStringArray G.genByteString
+        it "LByteStringArray" $ testBiMap B._LByteStringArray G.genLByteString
+
+    describe "Custom" $ do
+        it "EnumBounded (Ordering)" $ testBiMap B._EnumBounded $ Gen.enumBounded @_ @Ordering
+        it "Read (Integer)"         $ testBiMap B._Read G.genInteger
+        it "TextBy (Text)"          $ testBiMap (B._TextBy id Right) G.genText
+
+    describe "Key" $ do
+        it "KeyText"   $ testBiMap B._KeyText G.genKey
+        it "KeyString" $ testBiMap B._KeyString G.genKey
 
 testBiMap
     :: (Show a, Show b, Eq a)
