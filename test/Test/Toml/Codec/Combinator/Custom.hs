b242832330a96f661f8859572ac2daa497473f92
Dmitrii Kovanikov <kovanikov@gmail.com>
2020-05-17 17:17:47 +0100

    Add tests for 'textBy'

--- test/Test/Toml/Codec/Combinator/Custom.hs
+++ test/Test/Toml/Codec/Combinator/Custom.hs
@@ -2,10 +2,13 @@ module Test.Toml.Codec.Combinator.Custom
     ( customSpec
     ) where
 
+import Data.Bifunctor (first)
 import Test.Hspec (Spec, describe)
+import Text.Read (readEither)
 
 import Test.Toml.Codec.Combinator.Common (codecRoundtrip)
 
+import qualified Data.Text as Text
 import qualified Hedgehog.Gen as Gen
 import qualified Test.Toml.Gen as Gen
 import qualified Toml.Codec.BiMap.Conversion as Toml
@@ -19,3 +22,6 @@ customSpec = describe "Combinator.Custom: Roundtrip tests" $ do
     codecRoundtrip "Validate (Even) "
         (Toml.validateIf even Toml._Int)
         ((* 2) <$> Gen.genInt)
+    codecRoundtrip "TextBy          "
+        (Toml.textBy (Text.pack . show) (first Text.pack . readEither . Text.unpack))
+        Gen.genInt
