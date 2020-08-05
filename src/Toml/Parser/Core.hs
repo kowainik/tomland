4cd2a9493d37148eb34be030f96031f33567006c
DarioSindicic <dario.sindicic@gmail.com>
2020-08-05 15:22:10 +0200

    [#331] add Data.Char module for ascii value and parsec modules for hex,oct and bin digits

--- src/Toml/Parser/Core.hs
+++ src/Toml/Parser/Core.hs
@@ -26,7 +26,7 @@ import Data.Void (Void)
 
 import Text.Megaparsec (Parsec, anySingle, eof, errorBundlePretty, match, parse, satisfy, try,
                         (<?>))
-import Text.Megaparsec.Char (alphaNumChar, char, digitChar, eol, hexDigitChar, space, space1,
+import Text.Megaparsec.Char (alphaNumChar, char, digitChar, eol, hexDigitChar, octDigitChar, binDigitChar, space, space1,
                              string, tab)
 import Text.Megaparsec.Char.Lexer (binary, float, hexadecimal, octal, signed, skipLineComment,
                                    symbol)
@@ -52,3 +52,32 @@ lexeme = L.lexeme sc
 -- | 'Parser' for "fixed" string.
 text :: Text -> Parser Text
 text = symbol sc
+
+hexStringToInt :: String -> Int
+hexStringToInt [] = 0
+hexStringToInt (x:xs) = 16 * (hexChar x) + hexStringToInt xs 
+
+hexChar :: Char -> Int 
+hexChar c 
+   | c == '0' = 0
+   | c == '1' = 1
+   | c == '2' = 2
+   | c == '3' = 3
+   | c == '4' = 4
+   | c == '5' = 5
+   | c == '6' = 6
+   | c == '7' = 7
+   | c == '8' = 8
+   | c == '9' = 9
+   | c == 'a' = 10
+   | c == 'b' = 11
+   | c == 'c' = 12
+   | c == 'd' = 13
+   | c == 'e' = 14
+   | c == 'f' = 15
+   | c == 'A' = 10
+   | c == 'B' = 11
+   | c == 'C' = 12
+   | c == 'D' = 13
+   | c == 'E' = 14
+   | c == 'F' = 15
