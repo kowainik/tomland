d1b48b750067d548b492e295305c0316d317d932
DarioSindicic <dario.sindicic@gmail.com>
2020-11-18 10:51:31 +0100

    removed parsing and unparing tests

--- test/Test/Toml/Parser/Examples.hs
+++ test/Test/Toml/Parser/Examples.hs
@@ -1,7 +1,5 @@
 module Test.Toml.Parser.Examples
     ( examplesSpec
-    , parseUnparseSpec
-    , parseUnparse
     ) where
 
 import Data.Either (isRight)
@@ -22,18 +20,6 @@ example :: FilePath -> Spec
 example file = it ("can parse file " ++ file) $ do
     toml <- TIO.readFile (exampleDir ++ file)
     isRight (parse toml) `shouldBe` True
-
-parseUnparseSpec :: Spec
-parseUnparseSpec = describe "Can get same object after parse and unparse" $ do
-    files <- runIO $ listDirectory exampleDir
-    mapM_ parseUnparse files
-
-parseUnparse :: FilePath -> Spec
-parseUnparse file = it ("it will produce same text after parsing and unparsing " ++ file) $ do
-    toml <- TIO.readFile (exampleDir ++ file)
-    case parse toml of
-       Left _ -> error "File can't be parsed"
-       Right toml_obj -> (parse toml == ( parse $ pretty $ toml_obj)) `shouldBe` True
 
 exampleDir :: FilePath
 exampleDir = "test/examples/"
